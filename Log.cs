4ad4815e5d1fb992e39f5503f63f7a8968bb5d83
oorzkws <65210810+oorzkws@users.noreply.github.com>
2024-11-28 18:41:06 -0700

    Initial commit

---
+++ Log.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx.Logging;
+
+namespace ElinModTemplate;
+
+public static class Logging {
+    internal static void Log(object payload, LogLevel level = LogLevel.Info) {
+        // This doesn't error, Rider just hasn't caught up
+        ElinModTemplate.Logger.Log(level, payload);
+    }
+}
